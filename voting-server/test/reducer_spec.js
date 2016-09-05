adcd35eddbad227198aba9048c2024ed0d10df64
Erik Miller <emiller46@radford.edu>
2016-09-05 16:44:42 -0400

    tests for reducer and reducer implement.

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -6,7 +6,7 @@ import reducer from '../src/reducer';
 describe('Reducer', () => {
     it('handles SET_ENTRIES', () => {
         const initialState = Map();
-        const action = {type: 'SET_ENTRIES', entries ['Homer']};
+        const action = {type: 'SET_ENTRIES', entries: ['Homer']};
         const nextState = reducer(initialState, action);
 
         expect(nextState).to.equal(fromJS({
@@ -29,5 +29,30 @@ describe('Reducer', () => {
         }));
     });
 
-    it();
+    it('handles VOTE', () => {
+        const initialState = fromJS({
+            vote: {
+                pair: ['Homer', 'Marge']
+            },
+            entries: []            
+        });
+        const action = {type: 'VOTE', entry: 'Homer'};
+        const nextState = reducer(initialState, action);
+        
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Homer', 'Marge'],
+                tally: {Homer: 1}
+            },
+            entries: []
+        }));
+    });
+
+    it('has an initial state', () => {
+        const action = {type: 'SET_ENTRIES', entries: ['Homer']};
+        const nextState = reducer(undefined, action);
+        expect(nextState).to.equal(fromJS({
+            entries: ['Homer']
+        }));
+    });
 });

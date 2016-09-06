e902fc788d76e9985ad55f659763a7312f49a232
Erik Miller <emiller46@radford.edu>
2016-09-06 09:20:30 -0400

    added subscribe to store for each connecting socket client

---
+++ voting-server/src/server.js
@@ -0,0 +1,9 @@
+import Server from 'socket.io';
+
+export default function startServer(store){
+    const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS());
+    );
+}
